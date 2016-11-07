1efce9bb4ec1d3f6fac3fc377c2fb6879cd30221
keuhdall <lmarques@student.42.fr>
2016-11-07 18:36:16 +0100

    added struct for tetriminos, and renamed solver.c, which is now tetri.c and contains functions to feed datas to the struct

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/21 23:22:30 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/29 00:53:55 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/07 18:04:56 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,29 @@
 
 # include <stdio.h>
 # include <fcntl.h>
-# include "../libft/libft.h"
+# include "../../libft/libft.h"
 
-void			ft_lst_push_back(t_list **begin_list, t_list *elem);
-void			ft_print_list(t_list *lst);
-t_list			*ft_read_file(char *name, int *err);
-int				ft_is_valid(char *str);
-int				ft_get_height(char *str);
-int				ft_get_length(char *str);
+typedef struct			s_point
+{
+	int					x;
+	int					y;
+}						t_point;
+
+typedef struct			s_etri
+{
+	char				c;
+	int					width;
+	int					height;
+	t_point				pos;
+	t_point				blocs[4];
+	t_point				top;
+	t_point				bottom;
+}						t_etri;
+
+void					ft_print_list(t_list *lst);
+t_list					*ft_read_file(char *name, int *err);
+int						ft_is_valid(char *str);
+int						ft_get_width(const char *str);
+int						ft_get_height(const char *str);
 
 #endif
--- tetri.c
+++ tetri.c
@@ -6,13 +6,13 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 01:16:48 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/29 00:40:23 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/07 18:32:59 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int			ft_get_height(char *str)
+int		ft_get_height(const char *str)
 {
 	int	count;
 	int	count_height;
@@ -35,19 +35,52 @@ int			ft_get_height(char *str)
 	return (count_height);
 }
 
-int			ft_get_length(char *str)
+int		ft_get_width(const char *str)
 {
 	int	count;
-	int	count_length;
+	int	count_width;
 
 	count = 0;
-	count_length = 0;
+	count_width = 0;
 	while (count <= 4)
 	{
 		if (str[count] == '#' || str[count + 5] == '#' ||
 			str[count + 10] == '#' || str[count + 15] == '#')
-			count_length++;
+			count_width++;
 		count++;
 	}
-	return (count_length);
+	return (count_width);
+}
+
+t_point	*ft_get_blocs(const char *str)
+{
+	static t_point	bloc[4];
+	int				count_str;
+	int				count_blocs;
+
+	count_str = 0;
+	count_blocs = 0;
+	while (str[count_str])
+	{
+		if (str[count_str] == '#')
+		{
+			bloc[count_blocs].x = count_str % 5;
+			bloc[count_blocs].y = (count_str / 5) - 1;
+			count_blocs++;
+		}
+		count_str++;
+	}
+	return (bloc);
+}
+
+t_etri	ft_create_tetri(char *str)
+{
+	t_etri		tetri;
+	static char	c = 'A';
+
+	tetri.width = ft_get_width(str);
+	tetri.height = ft_get_height(str);
+	tetri.c = c;
+	c++;
+	return (tetri);
 }

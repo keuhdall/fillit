a317c4d5af6f035190bb22aa391d91bdaec54511
keuhdall <lmarques@student.42.fr>
2016-11-08 14:05:36 +0100

    moved ft_create_tetri from fillit.c to tetri.c

--- fillit.c
+++ fillit.c
@@ -6,36 +6,34 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 01:02:36 by lmarques          #+#    #+#             */
-/*   Updated: 2016/11/08 01:31:24 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/08 14:03:05 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-t_etri	ft_create_tetri(char *str)
-{
-	t_etri		tetri;
-	static char	c = 'A';
-
-	tetri.c = c;
-	tetri.width = ft_get_width(str);
-	tetri.height = ft_get_height(str);
-	tetri.pos = ft_get_pos(str);
-	tetri.blocs = ft_get_blocs(str);
-	c++;
-	return (tetri);
-}
-
 int		main(int argc, char *argv[])
 {
 	int		err;
+	t_etri	tetri;
 	t_list	*list;
 
 	err = 0;
 	list = NULL;
 	list = ft_read_file(argv[1], &err);
 	if (err != -1)
-		ft_print_list(list);
+	{
+		while (list)
+		{
+			if (ft_is_valid((char *)list->content))
+			{
+				tetri = ft_create_tetri((char *)list->content);
+				ft_print_tetri(tetri);
+			}
+			list = list->next;
+		}
+		//ft_print_list(list);
+	}
 	argc++;
 	return (0);
 }

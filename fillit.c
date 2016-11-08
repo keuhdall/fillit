d8c8d69c69eee9fca9144891cfbd4087651a789b
keuhdall <lmarques@student.42.fr>
2016-11-08 22:09:23 +0100

    added real main for proper tests; started the implementation of the map

--- fillit.c
+++ fillit.c
@@ -6,34 +6,62 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 01:02:36 by lmarques          #+#    #+#             */
-/*   Updated: 2016/11/08 14:03:05 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/08 21:48:06 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+char	*ft_init_map(char *map)
+{
+	int	count;
+
+	count = 0;
+	if (!(map = (char *)malloc(sizeof(char) * 16 * 16 + 1)))
+		return (NULL);
+	while (count <= (16 * 16))
+	{
+		map[count] = '.';
+		count++;
+	}
+	map[count] = '\0';
+	return (map);
+}
+
 int		main(int argc, char *argv[])
 {
-	int		err;
+	int		i;
+	t_etri	*tab;
 	t_etri	tetri;
 	t_list	*list;
+	char	*map;
 
-	err = 0;
+	i = 0;
+	if (!(tab = (t_etri *)malloc(sizeof(t_etri *) * 26)))
+		tab = NULL;
 	list = NULL;
-	list = ft_read_file(argv[1], &err);
-	if (err != -1)
+	list = ft_read_file(argv[1], &i);
+	map = NULL;
+	map = ft_init_map(map);
+	if (i == -1)
+		return (-1);
+	while (list)
 	{
-		while (list)
+		if (ft_is_valid((char *)list->content))
 		{
-			if (ft_is_valid((char *)list->content))
-			{
-				tetri = ft_create_tetri((char *)list->content);
-				ft_print_tetri(tetri);
-			}
-			list = list->next;
+			tetri = ft_create_tetri((char *)list->content);
+			ft_set_in_corner(&tetri, (char *)list->content);
+			i = ft_push_back_tetri(&tab, tetri);
 		}
-		//ft_print_list(list);
+		else
+		{
+			ft_putstr_fd("unvalid file (2)\n", 2);
+			return (-1);
+		}
+		list = list->next;
 	}
+	while (i--)
+		ft_print_tetri(tab[i]);
 	argc++;
 	return (0);
 }

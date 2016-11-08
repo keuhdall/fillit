09768f98ffc6819200887d42976a570445282b62
Léonard Marques <lmarques@student.42.fr>
2016-11-08 01:39:41 +0100

    added the ft_create_tetri function

--- fillit.c
+++ fillit.c
@@ -6,11 +6,25 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/23 01:02:36 by lmarques          #+#    #+#             */
-/*   Updated: 2016/09/29 00:41:09 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/11/08 01:31:24 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+t_etri	ft_create_tetri(char *str)
+{
+	t_etri		tetri;
+	static char	c = 'A';
+
+	tetri.c = c;
+	tetri.width = ft_get_width(str);
+	tetri.height = ft_get_height(str);
+	tetri.pos = ft_get_pos(str);
+	tetri.blocs = ft_get_blocs(str);
+	c++;
+	return (tetri);
+}
 
 int		main(int argc, char *argv[])
 {
